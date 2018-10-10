cfb884a2ba4c811603cb2aa3de91cf9a4fed2535
wangqijun <wangqj2011@sohu.com>
2018-10-10 10:49:57 +0800

    change pool implement

--- utils/pool.go
+++ utils/pool.go
@@ -1,152 +1,21 @@
 package utils
 
-import (
-	"io"
-	"time"
-	"sync"
-	"errors"
-	"net"
-	log "github.com/sirupsen/logrus"
-)
+import "errors"
 
 var (
-	ErrInvalidConfig = errors.New("invalid pool config")
-	ErrPoolClosed    = errors.New("pool closed")
+	//ErrClosed 连接池已经关闭Error
+	ErrClosed = errors.New("pool is closed")
 )
 
-type Poolable interface {
-	io.Closer
-	GetActiveTime() time.Time
-	GetConn() net.Conn
-}
-
-type factory func() (Poolable, error)
-
+//Pool 基本方法
 type Pool interface {
-	Acquire() (Poolable, error) // 获取资源
-	Release(Poolable) error     // 释放资源
-	Close(Poolable) error       // 关闭资源
-	Shutdown() error            // 关闭池
-}
+	Get() (interface{}, error)
 
-type GenericPool struct {
-	sync.Mutex
-	pool        chan Poolable
-	maxOpen     int  // 池中最大资源数
-	numOpen     int  // 当前池中资源数
-	minOpen     int  // 池中最少资源数
-	used        int  // 被使用数量
-	maxIdle     int  // 最大空闲数量
-	closed      bool // 池是否已关闭
-	maxLifetime time.Duration
-	factory     factory // 创建连接的方法
-}
+	Put(interface{}) error
 
-func NewGenericPool(minOpen, maxOpen int, maxLifetime time.Duration, factory factory) (*GenericPool, error) {
-	if maxOpen <= 0 || minOpen > maxOpen {
-		return nil, ErrInvalidConfig
-	}
-	p := &GenericPool{
-		maxOpen:     maxOpen,
-		minOpen:     minOpen,
-		maxLifetime: maxLifetime,
-		factory:     factory,
-		pool:        make(chan Poolable, maxOpen),
-	}
+	Close(interface{}) error
 
-	for i := 0; i < minOpen; i++ {
-		closer, err := factory()
-		if err != nil {
-			continue
-		}
-		p.numOpen++
-		p.pool <- closer
-	}
-	go stat(p)
-	return p, nil
-}
+	Release()
 
-func (p *GenericPool) Acquire() (Poolable, error) {
-	if p.closed {
-		return nil, ErrPoolClosed
-	}
-	for {
-		closer, err := p.getOrCreate()
-		if err != nil {
-			return nil, err
-		}
-
-		// 如果设置了超时且当前连接的活跃时间+超时时间早于现在，则当前连接已过期
-		if p.maxLifetime > 0 && closer.GetActiveTime().Add(p.maxLifetime).Before(time.Now()) {
-			p.Close(closer)
-			continue
-		}
-		return closer, nil
-	}
-}
-
-func (p *GenericPool) getOrCreate() (Poolable, error) {
-	select {
-	case closer := <-p.pool:
-		return closer, nil
-	default:
-	}
-	p.Lock()
-	if p.numOpen >= p.maxOpen {
-		closer := <-p.pool
-		p.Unlock()
-		return closer, nil
-	}
-	// 新建连接
-	closer, err := p.factory()
-	if err != nil {
-		p.Unlock()
-		return nil, err
-	}
-	p.numOpen++
-	p.Unlock()
-	return closer, nil
-}
-
-// 释放单个资源到连接池
-func (p *GenericPool) Release(closer Poolable) error {
-	if p.closed {
-		return ErrPoolClosed
-	}
-	p.Lock()
-	p.pool <- closer
-	p.Unlock()
-	return nil
-}
-
-// 关闭单个资源
-func (p *GenericPool) Close(closer Poolable) error {
-	p.Lock()
-	closer.Close()
-	p.numOpen--
-	p.Unlock()
-	return nil
-}
-
-// 关闭连接池，释放所有资源
-func (p *GenericPool) Shutdown() error {
-	if p.closed {
-		return ErrPoolClosed
-	}
-	p.Lock()
-	close(p.pool)
-	for closer := range p.pool {
-		closer.Close()
-		p.numOpen--
-	}
-	p.closed = true
-	p.Unlock()
-	return nil
-}
-
-func stat(p *GenericPool) {
-	for {
-		time.Sleep(10 * time.Second)
-		log.Println("pool stat -------numOpen=", p.numOpen, ",minOpen=", p.minOpen, ",maxOpen=", p.maxOpen)
-	}
+	Len() int
 }

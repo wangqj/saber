165db581f20460a236046dfd0ecd6e0b9edd4250
wangqijun <wangqj2011@sohu.com>
2018-10-21 22:39:40 +0800

    mock client

---
+++ cmd/proxy/main_mock.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"net"
+	"fmt"
+)
+
+func main() {
+	netListen, err := net.Listen("tcp", "127.0.0.1:16379")
+	if err != nil {
+		fmt.Println(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+	defer netListen.Close()
+	for i := 0; ; i++ {
+		fmt.Println(i)
+		conn, _ := netListen.Accept()
+
+		//fmt.Println(conn.RemoteAddr().String(), " tcp connect success")
+		go handle(conn)
+
+	}
+}
+
+func handle(proxyConn net.Conn) {
+	defer proxyConn.Close()
+	//fmt.Println("handle request")
+	for {
+
+		proxyBuffer := make([]byte, 2048)
+		proxyConn.Read(proxyBuffer)
+		//proxyBuffer = bytes.TrimRight(proxyBuffer, "\x00")
+		//fmt.Println("request content is %s", string(proxyBuffer))
+		proxyConn.Write([]byte("+OK\r\n"))
+		//time.Sleep(3*time.Second)
+
+		//fmt.Println("over")
+	}
+}
